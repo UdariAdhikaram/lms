ce2f26050fe38a92ebaf2dcc723cd5660577f886
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-23 00:48:59 +0530

    load dashboard

--- src/main/java/com/lms/DBConnection/DB.java
+++ src/main/java/com/lms/DBConnection/DB.java
@@ -1,7 +1,28 @@
 package com.lms.DBConnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DB {
 	
-	//public static Connection
+	public static Connection connect() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/leaenhub?useSSL=false","root","");
+			System.out.println("Connection successfull" + conn);
+			
+		}
+		catch(ClassNotFoundException | SQLException ex) {
+			System.err.println("Connection Failed");
+		}
+		
+		
+		
+		
+		return conn;
+	}
 }
 
--- src/main/java/com/lms/login/login.java
+++ src/main/java/com/lms/login/login.java
@@ -1,11 +1,20 @@
 package com.lms.login;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.lms.DBConnection.DB;
+import com.mysql.cj.protocol.Resultset;
 
 
 @WebServlet("/login")
@@ -13,7 +22,34 @@ public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello world");
+		String un = request.getParameter("username");
+		String pass = request.getParameter("password");
+		HttpSession session = request.getSession();
+		RequestDispatcher dispatcher = null;
+		
+		try {
+			Connection conn = DB.connect();
+			PreparedStatement pst = conn.prepareStatement("SELECT username,role,firstname,lastname FROM users WHERE username=? AND password=? AND status='active'");
+			pst.setString(1, un);
+			pst.setString(2, pass);
+			
+			ResultSet rs = pst.executeQuery();		
+			
+			if(rs.next()) {
+				int role = rs.getInt("role");
+				if(role==1) {
+					dispatcher = request.getRequestDispatcher("admin_dashboard/index.jsp");
+				}
+			 }else {
+					request.setAttribute("status", "failed");
+					dispatcher = request.getRequestDispatcher("index.jsp");
+				}
+			 	
+				dispatcher.forward(request, response);
+			}
+		catch(Exception e) {
+			
+		}
 	}
 
 }
