065270d31ccb3de42da90cafd07ad812d002e386
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-22 22:27:21 +0530

    import mysql-connector

--- src/main/java/com/lms/login/login.java
+++ src/main/java/com/lms/login/login.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class login
- */
+
 @WebServlet("/login")
 public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
